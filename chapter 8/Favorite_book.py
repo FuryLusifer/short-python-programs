5110e386b934736aff9b879a5c087b923931bdc3
FuryLusifer <piyushkayastha2@gmail.com>
2025-07-17 15:01:51 +0545

    started learning functions

---
+++ chapter 8/Favorite_book.py
@@ -0,0 +1,12 @@
+# Write a function called favorite_book() that accepts one parameter, title.
+# The function should print a message, such as One of my favorite books is
+# Alice in Wonderland. Call the function, making sure to
+# include a book title as an argument in the function call.
+
+def favorite_book(title):
+    """ This function prints my favorite book's name, which is pass it from
+    function call below as a argument. """
+
+    print(f"My favorite book is {title}.")
+
+favorite_book("Alice in Wonderland")
---
+++ chapter 8/message.py
@@ -0,0 +1,12 @@
+# Write a function called display_message() that prints one sentence telling
+# everyone what you are learning about in this chapter. Call the function,
+# and make sure the message displays correctly.
+
+def display_message():
+    ''' This is a docstring.
+        This functions prints a simple message.
+    '''
+    print("I am learning about functions in Python.")
+
+
+display_message()
